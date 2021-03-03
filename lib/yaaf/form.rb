8336d47fdd605c18344ed11f1eb1f3287657c916
Santiago Bartesaghi <santiago.bartesaghi@rootstrap.com>
2021-03-03 20:31:43 -0300

    Improve error handling (#56)

--- lib/yaaf/form.rb
+++ lib/yaaf/form.rb
@@ -30,26 +30,6 @@ module YAAF
 
     attr_accessor :models
 
-    def promote_errors(model)
-      if rails_version_less_than_6_1?
-        promote_legacy_errors(model)
-      else
-        model.errors.each do |model_error|
-          errors.add(model_error.attribute, model_error.type, **model_error.options)
-        end
-      end
-    end
-
-    def promote_legacy_errors(model)
-      model.errors.details.each do |attribute, details|
-        details.each do |detail|
-          options = detail.except(:error)
-
-          errors.add(attribute, detail[:error], **options)
-        end
-      end
-    end
-
     def save_in_transaction(options)
       ::ActiveRecord::Base.transaction do
         run_callbacks :save do
@@ -65,19 +45,16 @@ module YAAF
     end
 
     def validate_models
-      models.each do |model|
-        promote_errors(model) if model.invalid?
-      end
+      models.each { |model| promote_errors(model) if model.invalid? }
+    end
+
+    def promote_errors(model)
+      errors.merge!(model.errors)
     end
 
     def handle_transaction_rollback(exception)
       run_callbacks :rollback
       raise exception
-    end
-
-    def rails_version_less_than_6_1?
-      ActiveModel::VERSION::MAJOR < 6 ||
-        ActiveModel::VERSION::MAJOR == 6 && ActiveModel::VERSION::MINOR.zero?
     end
   end
 end
