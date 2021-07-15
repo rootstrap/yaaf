56e6bbcb1b26d57d36bea835ea69f67080a8f330
Nathaniel Watts <reg@nathanielwatts.com>
2021-07-15 09:54:48 -0500

    Prevent double validations on save/save! (#70)
    * Prevent double validations on save/save!
    
    Originally, calling `form.save/!` would cause model validations to fire
    off twice.
    
    The first time when the form's validations are being run (as the form
    has a validation to ensure that all `models` are valid`)
    
    The second time is when the `models` are looped over and they each
    receive `save!`.
    
    For validations that are done with in-memory data, such as `validate
    :attribute, presence: true`, this isn't a big deal. But this could be
    problematic for any validation that is dependent on the state of the
    database (it will effectively hit the database twice).
    
    This instead passes `validate: false` into the saving of models so that
    their validations aren't run a second time.
    
    * Update README to reflect line count

--- spec/support/models/user.rb
+++ spec/support/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   validates :name, presence: true
+
+  validate :custom_validation
+
+  def custom_validation
+    'A validation method used in the specs'
+  end
 end
