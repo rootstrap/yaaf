4c9ea35ae708802191432ee713a84ebe89c6c235
Santiago Bartesaghi <santib@hey.com>
2024-07-05 18:04:36 -0300

    Update Ruby and Rails versions (#86)

--- spec/support/database_cleaner_monkeypatch.rb
+++ spec/support/database_cleaner_monkeypatch.rb
@@ -2,7 +2,9 @@ module DatabaseCleaner
   module ActiveRecord
     class Base < DatabaseCleaner::Strategy
       def self.migration_table_name
-        if Gem::Version.new('6.0.0') <= ::ActiveRecord.version
+        if ::ActiveRecord.version >= Gem::Version.new('7.2.0.beta2')
+          ::ActiveRecord::Base.connection_pool.schema_migration.table_name
+        elsif ::ActiveRecord.version >= Gem::Version.new('6.0.0')
           ::ActiveRecord::Base.connection.schema_migration.table_name
         else
           ::ActiveRecord::SchemaMigration.table_name
