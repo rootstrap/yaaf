09d69c6b5887a845445b460eaf4b12cb87505969
Andres Garcia <andres@rootstrap.com>
2026-01-30 18:13:36 -0300

    Fix

--- lib/yaaf/form.rb
+++ lib/yaaf/form.rb
@@ -7,7 +7,11 @@ module YAAF
     include ::ActiveModel::Validations::Callbacks
     include ::ActiveRecord::Transactions
     include ::ActiveModel::Attributes
-    include ::ActiveModel::Attributes::Normalization if defined?(::ActiveModel::Attributes::Normalization)
+
+    if defined?(::ActiveModel::Attributes::Normalization)
+      include ::ActiveModel::Attributes::Normalization
+    end
+
     include ::ActiveRecord::Normalization if defined?(::ActiveRecord::Normalization)
 
     define_model_callbacks :save
