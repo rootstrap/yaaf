8663671476801e642717cf4e97144544abaec6cf
Andres Garcia <andres@rootstrap.com>
2026-02-10 11:15:32 -0300

    Test form with attributes

---
+++ spec/yaaf/attributes_spec.rb
@@ -0,0 +1,76 @@
+# frozen_string_literal: true
+
+RSpec.describe 'Form with attributes' do
+  let(:options) { {} }
+  let(:registration_form) { WithAttributesForm.new(attributes) }
+  let(:attributes) { { email: 'test@example.com', name: 'John' } }
+
+  before { expect(registration_form).to be_valid }
+
+  describe '#save' do
+    subject { registration_form.save(options) }
+
+    it 'returns true' do
+      expect(subject).to eq true
+    end
+
+    it 'saves the user' do
+      expect { subject }.to change { User.count }.by 1
+    end
+
+    it 'saves with correct information' do
+      expect { subject }.to change {
+        User.last&.email
+      }.to('test@example.com').and change {
+        User.last&.name
+      }.to('John')
+    end
+  end
+
+  describe '#save!' do
+    subject { registration_form.save!(options) }
+
+    it 'returns true' do
+      expect(subject).to eq true
+    end
+
+    it 'saves the user' do
+      expect { subject }.to change { User.count }.by 1
+    end
+
+    it 'saves with correct information' do
+      expect { subject }.to change {
+        User.last&.email
+      }.to('test@example.com').and change {
+        User.last&.name
+      }.to('John')
+    end
+  end
+
+  describe '#valid?' do
+    subject { registration_form.valid? }
+
+    it { is_expected.to be true }
+  end
+
+  describe '#invalid?' do
+    subject { registration_form.invalid? }
+
+    it { is_expected.to be false }
+  end
+
+  describe '#errors' do
+    subject do
+      registration_form.valid?
+      registration_form.errors
+    end
+
+    it 'returns the correct class' do
+      expect(subject.class).to eq(ActiveModel::Errors)
+    end
+
+    it 'is empty' do
+      expect(subject.messages).to be_empty
+    end
+  end
+end
